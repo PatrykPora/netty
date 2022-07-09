d3f4f77a1b6ce53419458f441f511fa1fbf8b279
unknown <patryk_pora@o2.pl>
2022-07-09 20:40:51 +0200

    dicard sever handler

---
+++ src/main/java/DiscardServerHandler.java
@@ -0,0 +1,36 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+//DiscardServerHandler extends ChannelInboundHandlerAdapter,
+// which is an implementation of ChannelInboundHandler.
+// ChannelInboundHandler provides various event handler methods that you can override.
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+
+    //We override the channelRead() event handler method here.
+    // This method is called with the received message,
+    // whenever new data is received from a client.
+    // In this example, the type of the received message is ByteBuf.
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // we just discard received data here
+        // in normal scenario ByteBuf has to be release it is handler responsibility any object passed to the handler
+        //A random and sequential accessible sequence of zero or more bytes (octets).
+        //  ByteBuf This abstract class provides an abstract view for one or more primitive byte arrays (byte[]) and NIO buffers.
+        ((ByteBuf)msg).release();
+    }
+
+    //The exceptionCaught() event handler method is called with a Throwable
+    // when an exception was raised by Netty due to an I/O error
+    // or by a handler implementation due to the exception thrown while processing events.
+    // In most cases, the caught exception should be logged and its associated channel should be closed
+    // here, although the implementation of this method can be different depending on what you want to do
+    // to deal with an exceptional situation. For example, you might want to send a response message with an error code
+    // before closing the connection.
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
